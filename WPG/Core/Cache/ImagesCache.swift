8d358496c69039859c391e151ffb79ccf0f90908
jordg2015 <jordy.g2012@gmail.com>
2021-05-23 22:42:14 -0600

    feat(gallery): set cache limit

--- WPG/Core/Cache/ImagesCache.swift
+++ WPG/Core/Cache/ImagesCache.swift
@@ -10,17 +10,24 @@ import Foundation
 class ImagesCache {
     
     static let `default` = ImagesCache()
-    
+    let maxPhotosCache: Int = 100
+    let maxProfileImagesCache: Int = 300
     private var photos: [String : Data] = [:]
     private var profileImages: [String : Data] = [:]
     
     func set(key: String, photo: Data?) {
         guard let data = photo else { return }
+        if photos.count >= maxPhotosCache {
+            photos.removeAll()
+        }
         photos[key] = data
     }
     
     func set(key: String, profileImage: Data?) {
         guard let data = profileImage else { return }
+        if profileImages.count >= maxProfileImagesCache {
+            profileImages.removeAll()
+        }
         profileImages[key] = data
     }
     
